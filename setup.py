42f0f99f49a25a92f4a8d079dc88cadf4652ec3b
JanZ <janzuiderveld@gmail.com>
2021-12-02 19:20:52 +0100

    Bugfixes

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'INR_collection',
   packages = ['INR_collection'],
-  version = '0.2',
+  version = '0.3',
   license='MIT',
   description = 'A collection of (conditional/modulatable) implicit neural representation implementations and basic building blocks in PyTorch.',
   author = 'Jan Zuiderveld',
   author_email = 'janzuiderveld@gmail.com',
   url = 'https://github.com/janzuiderveld/INR-collection',
-  download_url = 'https://github.com/janzuiderveld/INR-collection/archive/v_02.tar.gz',
-  keywords = ['Implicit neural representations', 'functional', "continouos", 'SIREN' , 'IM-NET'],
+  download_url = 'https://github.com/janzuiderveld/INR-collection/archive/v_03.tar.gz',
+  keywords = ['Implicit neural representations', 'functional', "continuous", 'SIREN' , 'IM-NET'],
   install_requires=[
           'numpy',
           'torch'
